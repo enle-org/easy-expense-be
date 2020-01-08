fca43de7812f3154c58148fd474568421aaabbf8
Ekpo David <dave170e71@gmail.com>
2020-01-08 15:15:33 +0100

    org

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const users = require('./users/users.service.js');
+const organisation = require('./organisation/organisation.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
+  app.configure(organisation);
 };
